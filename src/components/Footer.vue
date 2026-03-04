155fd22f9125301035fc206bb8437d2df65da6be
nadiri <nadiri@gmail.com>
2026-03-04 16:29:59 +0300

    updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,6 +21,7 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link to="/solutions">Solutions</router-link></li>
             <li><router-link to="/case-studies">Case Studies</router-link></li>
+            <li><router-link to="/blog">Blog</router-link></li>
             <li><router-link to="/contact">Contact</router-link></li>
           </ul>
         </div>
